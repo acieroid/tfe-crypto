f66641498e1431e916e6b6847a1eed30d7019536
Quentin Stievenart (acieroid) <quentin@tux.(none)>
2008-12-27 00:09:51 +0100

    Changements mineurs, ajouts d'un schéma de la grille de cardan

--- texte/src/rot13.lisp
+++ texte/src/rot13.lisp
@@ -6,16 +6,16 @@
 (defun rot13 (string)
   (labels 
       ((rot13-rec (string acc)
-		  (if (null string)
-		      (reverse acc)
-		    (let ((letter (car string)))
-		      (rot13-rec (cdr string)
-				 (cons 
-				  ; Soit la lettre codee, 
-				  ; soit le meme caractere special
-				  (if (member letter *alphabet*)
-				      (nth (position letter *alphabet*)
-					   *coded-alphabet*)
-				    letter)
-				  acc))))))
+	 (if (null string)
+	     (reverse acc)
+	     (let ((letter (car string)))
+	       (rot13-rec (cdr string)
+			  (cons 
+			   ; Soit la lettre codee, 
+			   ; soit le meme caractere special
+			   (if (member letter *alphabet*)
+			       (nth (position letter *alphabet*)
+				    *coded-alphabet*)
+			       letter)
+			   acc))))))
     (coerce (rot13-rec (coerce string 'list) '()) 'string)))

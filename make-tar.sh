d8e98668e188e04014c57aaa96b8bafde9e730f1
Quentin Stievenart (acieroid) <quentin@tux.(none)>
2008-12-30 20:20:14 +0100

    Suite de l'histoire. À partir de la première guerre mondiale à faire

--- make-tar.sh
+++ make-tar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILES="texte/*.tex texte/annexes/*.tex texte/bibliographie/*.bib texte/contenu/*.tex texte/images/*.png texte/dia/*.dia texte/src/README texte/src/*.c texte/src/*.hs texte/src/*.scm texte/src/*.py texte/src/*.lisp texte/eps/*.eps texte/plan texte/todo"
+FILES="texte/Makefile texte/*.tex texte/annexes/*.tex texte/bibliographie/*.bib texte/contenu/*.tex texte/images/*.png texte/images/*.svg texte/images/*.jpg texte/dia/*.dia texte/src/README texte/src/*.c texte/src/*.hs texte/src/*.scm texte/src/*.py texte/src/*.lisp texte/plan texte/todo texte/sourcesimages"
 
 tar -czvf "tfe-`date +\"%d-%m-%y\"`.tar.gz" $FILES
 #echo $FILES

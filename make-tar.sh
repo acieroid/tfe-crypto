f788f2a60dbc4be1530481f8c176859f564f989f
Quentin Stievenart (acieroid) <quentin@tux.(none)>
2009-01-17 13:04:07 +0100

    Simplification de certaines images. Seconde guerre mondiale presque terminée

--- make-tar.sh
+++ make-tar.sh
@@ -2,7 +2,10 @@
 
 FILES="texte/Makefile texte/*.tex texte/annexes/*.tex texte/bibliographie/*.bib texte/contenu/*.tex texte/images/*.png texte/images/*.svg texte/images/*.jpg texte/dia/*.dia texte/src/README texte/src/*.c texte/src/*.hs texte/src/*.scm texte/src/*.py texte/src/*.lisp texte/plan texte/todo texte/sourcesimages"
 
+TEXTES="texte/Makefile texte/*.tex texte/annexes/*.tex texte/bibliographie/*.bib texte/contenu/*.tex texte/images/*.svg"
+
 ARCHIVE="tfe-`date +\"%d-%m-%y\"`.tar.gz"
+TEXARCHIVE="tfe-tex-`date +\"%d-%m-%y\"`.tar.gz"
 GMAILFSPATH=/mnt/gmailfs
 SUBDIR=tfe/
 TUXFAMILYADDR=ssh.tuxfamily.org
@@ -11,11 +14,12 @@ TUXFAMILYPATH=/home/acieroidweb/acieroid.tuxfamily.org-web/htdocs/tfe/archives/
 
 echo "Packaging ..."
 tar -czvf $ARCHIVE $FILES
-echo "Sending on gmailfs"
-sudo mount.gmailfs none $GMAILFSPATH
-sudo cp $ARCHIVE $GMAILFSPATH/$SUBDIR
+tar -czvf $TEXARCHIVE $TEXTES
+#echo "Sending on gmailfs"
+#sudo mount.gmailfs none $GMAILFSPATH
+#sudo cp $ARCHIVE $GMAILFSPATH/$SUBDIR
 
 echo "Scping to tuxfamily"
-scp $ARCHIVE $TUXFAMILYUSER@$TUXFAMILYADDR:$TUXFAMILYPATH
+scp $TEXARCHIVE $TUXFAMILYUSER@$TUXFAMILYADDR:$TUXFAMILYPATH
 
 echo "Ok"
